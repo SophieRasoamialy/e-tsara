118493a8ade0b1ed7e28063aae6b9df16d5701c5
Sophie Hasindrae <sophiehasindrae@gmail.com>
2024-10-15 20:00:44 +0300

    changement de mongo deploy

--- autograder-backend/server.js
+++ autograder-backend/server.js
@@ -42,7 +42,7 @@ app.use(cors({
 }));
 
 app.use(cookieParser());
-
+console.log("mongo uri", process.env.MONGO_URI)
 // Connexion à MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
