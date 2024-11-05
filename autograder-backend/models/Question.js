873e568d20bd297326f71494f4865364b0e0f8f9
Sophie Hasindrae <sophiehasindrae@gmail.com>
2024-11-05 21:03:33 +0300

    pour tester

--- autograder-backend/models/Question.js
+++ autograder-backend/models/Question.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const questionSchema = new mongoose.Schema({
     exam_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Exam', required: true },
     text: { type: String, required: true },
-    answer_type: { type: String, enum: ['texte', 'QCM', 'vrai ou faux', 'compléter', 'conception'], required: true },
+    answer_type: { type: String, enum: ['texte', 'QCM', 'vrai ou faux', 'diagramme', 'image'], required: true },
     answer_duplicated: { type: Boolean, default: false },
     points: { type: Number, required: true },
   }, {
